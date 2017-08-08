7e512ea08b5b29b3e6279c559efefc9b836916ce
yochie <yoann.ochietti@gmail.com>
2017-08-08 17:51:18 -0400

    Organized and commented code. Removed some useless code.

--- scripts/music_bot.py
+++ scripts/music_bot.py
@@ -14,30 +14,16 @@ from tkinter import *
 
 experimental = False
 
-def main():
-	settings = music21.environment.UserSettings()
 
-	settings['lilypondPath'] = r'D:\\Apps\\LilyPond\\usr\\bin\\lilypond.exe'
+def main():
 	root = Tk()
 	recordingGui = RecordingGui(root)
 	root.mainloop()
-
-	#When record pressed,
-		#recordMidi()
-
-	#When Upload pressed,
-		#open form window
-
-	#when Done pressed
-		#upload(form)
 
 class RecordingGui:
 	def __init__(self, master):
 		self.master = master
 		master.title("Record music!")
-
-		self.label = Label(master, text="Recording time: 00:00")
-		self.label.pack()
 
 		self.greet_button = Button(master, text="Start recording", command=self.recordStart)
 		self.greet_button.pack()
@@ -45,6 +31,11 @@ class RecordingGui:
 		self.greet_button = Button(master, text="End recording", command=self.recordEnd)
 		self.greet_button.pack()
 
+		#Chronometer - TODO
+		# self.label = Label(master, text="Recording time: 00:00")
+		# self.label.pack()
+
+		#Seperate button for upload
 		# self.greet_button = Button(master, text="Upload", command=self.openForm)
 		# self.greet_button.pack()
 
@@ -53,16 +44,17 @@ class RecordingGui:
 
 	def recordStart(self):
 		print("start rec!")
-		#initialize empty file
-		#self.track = MidiTrack()
-		#self.mid.tracks.append(self.track)
+
+		#initialize structures used to record
 		self.previousTime = time.time()
+
 		self.mid = music21.midi.MidiFile()
 		self.mid.ticksPerQuarterNote = 1024
+
 		self.track = music21.midi.MidiTrack(0)
 		self.mid.tracks.append(self.track)
-		self.first=True
 
+		self.first=True
 
 		#open ports
 		self.inport = mido.open_input()
@@ -80,7 +72,7 @@ class RecordingGui:
 			if (self.first) :
 				experimentalDelta =1024
 				self.first = False
-			#ROUNDING to 0, 256, and multiples of 128
+			#rounding to 0, 256, and multiples of 128 thereafter
 			if experimentalDelta < 8 :
 				experimentalDelta = 0
 			else :
@@ -95,8 +87,8 @@ class RecordingGui:
 					if experimentalDelta > 4096 :
 						experimentalDelta = 4096
 
+			#for debug
 			print(experimentalDelta)
-			print(msg.type)
 
 		#FIXED TIMING VERSION
 		else :
@@ -104,8 +96,6 @@ class RecordingGui:
 				experimentalDelta = 0
 			else :
 				experimentalDelta = 1024
-		print(msg.type)
-
 
 		#DELTA TIME MSG
 		dt = music21.midi.DeltaTime(self.track)
@@ -122,49 +112,36 @@ class RecordingGui:
 		me1.channel = 1
 		self.track.events.append(me1)
 
-
+		#update previousTime
 		self.previousTime = currentTime
 
+		#for debug
 		print(me1)
 		
 
 	def recordEnd(self):
 		print("end rec!")
 
-		# add end of track
+		#END OF TRACK
 		dt = music21.midi.DeltaTime(self.track)
 		dt.time = 0
 		self.track.events.append(dt)
-
 		me = music21.midi.MidiEvent(self.track)
 		me.type = "END_OF_TRACK"
 		me.channel = 1
 		me.data = '' # must set data to empty string
 		self.track.events.append(me)
 		print(self.mid)
-		print(self.mid.writestr())
 
-
-
+		#close port
 		self.inport.callback = None
-
 		self.inport.close()
 
+		#Create MIDI file  from stream
 		self.mid.open('new_song.mid', 'wb')
-
 		self.mid.write()
-
 		self.mid.close()
-		
-		mf = music21.midi.MidiFile()
-		
-		mf.open('new_song.mid','rb')
-
-		mf.read()
-
-		mf.close()
-
-		stream = music21.midi.translate.midiFileToStream(mf)
+		stream = music21.midi.translate.midiFileToStream(self.mid)
 		stream.show()
 
 		#Create score PNG file
@@ -173,7 +150,7 @@ class RecordingGui:
 		filepath = 'D:/Apps/pywikibot/core/' + scorename
 		conv.write(stream, fmt = 'lilypond', fp=filepath, subformats = ['png'])
 
-		#Open form window
+		#Open form window to input title and launch upload
 		self.newWindow = Toplevel()
 		self.formGui = FormGui(self.newWindow)
 
@@ -208,49 +185,52 @@ class FormGui:
 		#upload fichier score
 		upload.main('-always','-filename:' + self.title + '.png', '-ignorewarn', '-noverify','new_score.png','''{{Fichier|Concerne=''' + self.title +'''|Est un fichier du type=Score}}''')
 
-
+		#Open page on wiki to input more info
 		webbrowser.open("http://leviolondejos.wiki/index.php?title=Spécial:AjouterDonnées/Enregistrement/" + self.title)
-		#uploadToWiki()
+
+		#close
 		self.master.destroy()
-
-def uploadToWiki():
-	path = os.path.abspath('../scripts/dict.txt')
-	f = open(path, 'w')
-	f.write(
-		"""xxxx
-		'''Upload test page'''
-		{{Interprétation
-		|A la partition -> Nom du fichier PDF/PNG du score uploadé
-		|A le fichier midi -> Nom du fichier MID uploadé
-		|A la description de l interprétation -> "Enregistré pendant WikiMania 2017"
-		}}
-		{{Interprète d une interprétation
-		|A l interprète -> GUI:Interprète
-		|Joue de l instrument -> GUI:Instrument
-		}}
-		{{Interprétation B}}
-		{{Morceau d une interprétation
-		|A le morceau interprété -> GUI:Morceau
-		|Est joué dans la tonalité -> Tonalité de music21
-		|A la métrique musicale -> Time signa9ture de music21
-		|A le bpm -> BPM moyen de music21(?)
-		}}
-		{{Interprétation C}}
-
-		<score lang="ABC">
-		X:1
-		M:C
-		L:1/4
-		K:C
-		C, D, E, F,|G, A, B, C|D E F G|A B c d|
-		e f g a|b c' d' e'|f' g' a' b'|]
-		</score>
-		yyyy"""
-		)
-	f.close()
-
-	pagefromfile.main('-begin:xxxx','-end:yyyy', '-notitle', '-force') #REMOVE FORCE AFTER TESTING
 
 if __name__ == '__main__':
 	main()
+
+
+#FOR ALTERNATIVE METHOD
+# def uploadToWiki():
+# 	path = os.path.abspath('../scripts/dict.txt')
+# 	f = open(path, 'w')
+# 	f.write(
+# 		"""xxxx
+# 		'''Upload test page'''
+# 		{{Interprétation
+# 		|A la partition -> Nom du fichier PDF/PNG du score uploadé
+# 		|A le fichier midi -> Nom du fichier MID uploadé
+# 		|A la description de l interprétation -> "Enregistré pendant WikiMania 2017"
+# 		}}
+# 		{{Interprète d une interprétation
+# 		|A l interprète -> GUI:Interprète
+# 		|Joue de l instrument -> GUI:Instrument
+# 		}}
+# 		{{Interprétation B}}
+# 		{{Morceau d une interprétation
+# 		|A le morceau interprété -> GUI:Morceau
+# 		|Est joué dans la tonalité -> Tonalité de music21
+# 		|A la métrique musicale -> Time signa9ture de music21
+# 		|A le bpm -> BPM moyen de music21(?)
+# 		}}
+# 		{{Interprétation C}}
+
+# 		<score lang="ABC">
+# 		X:1
+# 		M:C
+# 		L:1/4
+# 		K:C
+# 		C, D, E, F,|G, A, B, C|D E F G|A B c d|
+# 		e f g a|b c' d' e'|f' g' a' b'|]
+# 		</score>
+# 		yyyy"""
+# 		)
+# 	f.close()
+
+# 	pagefromfile.main('-begin:xxxx','-end:yyyy', '-notitle', '-force') #REMOVE FORCE AFTER TESTING
 

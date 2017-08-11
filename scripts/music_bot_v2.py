0a50b744e61266752e3ca4d249439c3301972d31
yochie <yoann.ochietti@gmail.com>
2017-08-11 13:07:47 -0400

    add putthrottle to sleep less

--- scripts/music_bot_v2.py
+++ scripts/music_bot_v2.py
@@ -271,10 +271,10 @@ class FormGui:
 		self.title = self.titleString.get()
 
 		#upload fichier MIDI
-		upload.main('-always','-filename:' + self.title + '.mid', '-ignorewarn', '-noverify','new_song.mid','''{{Fichier|Concerne=''' + self.title +'''|Est un fichier du type=MIDI}}''')
+		upload.main('-always','-filename:' + self.title + '.mid', '-ignorewarn', '-noverify','new_song.mid','-putthrottle:1','''{{Fichier|Concerne=''' + self.title +'''|Est un fichier du type=MIDI}}''')
 
 		#upload fichier score
-		upload.main('-always','-filename:' + self.title + '.png', '-ignorewarn', '-noverify','new_score.png','''{{Fichier|Concerne=''' + self.title +'''|Est un fichier du type=Score}}''')
+		upload.main('-always','-filename:' + self.title + '.png', '-ignorewarn', '-noverify','new_score.png','-putthrottle:1','''{{Fichier|Concerne=''' + self.title +'''|Est un fichier du type=Score}}''')
 
 		#Open page on wiki to input more info
 		webbrowser.open("http://leviolondejos.wiki/index.php?title=Spécial:AjouterDonnées/Enregistrement/" + self.title)
